b8c6058443f6a17b05f22d1d5042d13639e06aba
Caroline Tan <carolinectan3@gmail.com>
2021-07-24 17:02:07 -0400

    Add sort courses and students by name to courses index page

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
 
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def self.sort_by_name
+    order(:name)
   end
+end
--- spec/features/courses/index_spec.rb
+++ spec/features/courses/index_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'courses index page' do
     StudentCourse.create!(course: course2, student: harry)
     StudentCourse.create!(course: course3, student: draco)
     StudentCourse.create!(course: course3, student: cedric)
-    StudentCourse.create!(course: course2, student: harry)
+    StudentCourse.create!(course: course2, student: draco)
 
     visit '/courses'
 
@@ -38,5 +38,46 @@ RSpec.describe 'courses index page' do
       expect(page).to have_content(course3.students.length)
       expect(page).to have_content("Herbology: 2")
     end
+  end
+
+  it 'shows courses in alphabetical order' do
+    course1 = Course.create!(name: 'Charms')
+    course2 = Course.create!(name: 'Potions')
+    course3 = Course.create!(name: 'Herbology')
+
+    visit '/courses'
+
+    expect('Charms').to appear_before('Herbology')
+    expect('Herbology').to appear_before('Potions')
+  end
+
+  it 'shows students per course in alphabetical order' do
+    harry = Student.create!(name: 'Harry Potter', age: 14, house: 'Gryffindor')
+    draco = Student.create!(name: 'Draco Malfoy', age: 14, house: 'Slytherin')
+
+    course1 = Course.create!(name: 'Charms')
+    course2 = Course.create!(name: 'Potions')
+
+    StudentCourse.create!(course: course1, student: harry)
+    StudentCourse.create!(course: course2, student: harry)
+    StudentCourse.create!(course: course2, student: draco)
+
+    visit '/courses'
+save_and_open_page
+    within "#course-#{course1.id}" do
+      expect('Charms: 1').to appear_before('Harry Potter')
+    end
+
+    within "#course-#{course2.id}" do
+      expect('Potions: 2').to appear_before('- Draco Malfoy')
+      expect('- Draco Malfoy').to appear_before('- Harry Potter')
+    end
+
+
+  # (e.g. "Defense Against the Dark Arts: 3"
+  #       "- Elizabeth Burke"
+  #       "- Phineas Black"
+  #       "- Russell Lucky"
+  #       )
   end
 end
